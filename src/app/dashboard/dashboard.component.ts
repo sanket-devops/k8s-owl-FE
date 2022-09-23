d5a86f3b26984c18e3df053e1afd12225354b8f2
Sanket Patel <sanket.devops@gmail.com>
2022-09-23 16:19:49 -0400

    Sanket: Implement view and download pod logs.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../service/dashboard.service';
 import { Router } from '@angular/router';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Idashboard, ICluster } from '../interface/Idashboard';
 import { ConstantService } from '../service/constant.service';
+import { saveAs } from 'file-saver';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   responseData: any = undefined;
   clusterData: any = undefined;
+  clusterName: any = undefined;
+  podLogs: any = undefined;
+  groupId: any = undefined;
+  clusterId: any = undefined;
+  podName: any = undefined;
+  showPortal: boolean = false;
 
   constructor(
     public constantService: ConstantService,
     public dashboardService: DashboardService,
     public router: Router,
     private http: HttpClient,
-    
-    ) { }
+
+  ) { }
   async ngOnInit() {
     this.getAll();
 
@@ -28,30 +36,56 @@ export class DashboardComponent implements OnInit {
   async getAll() {
     let res = [];
     try {
-      res = <any>this.dashboardService.getClusters().subscribe((data: any)=> {
+      res = <any>this.dashboardService.getClusters().subscribe((data: any) => {
         this.responseData = data.data;
-        console.log(this.responseData);
-    });
+        // console.log(this.responseData);
+      });
     } catch (e) {
       console.log(e);
     }
   }
-  async getCluster(groupId: string, clusterId: string) {
+  async getCluster(groupId?: string, clusterId?: string, clusterName?: string, podName?: string) {
     this.clusterData = undefined;
+    this.clusterName = clusterName;
+    this.groupId = groupId;
+    this.clusterId = clusterId;
     let res = [];
     try {
-      res = <any>this.dashboardService.getPods( '/' + groupId, '/' + clusterId).subscribe((data: any)=> {
-        console.log(data);
+      res = <any>this.dashboardService.getPods('/' + groupId, '/' + clusterId).subscribe((data: any) => {
+        // console.log(data);
         this.clusterData = data;
-    });
+      });
     } catch (e) {
       console.log(e);
     }
   }
+  async getPodLogs(podName: string) {
+    this.podLogs = undefined;
+    this.podName = podName;
+    let res = [];
+    try {
+        this.podLogs = this.dashboardService.getPodsLogs('/' + this.groupId, '/' + this.clusterId, '/' + this.podName);
+        window.open(this.podLogs, "", "toolbar=yes,scrollbars=yes,resizable=yes,top=1000,left=1000,width=1000,height=1000");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async downloadLogs(podName: string) {
+    this.podLogs = undefined;
+    this.podName = podName;
+    let res = [];
+    try {
+        this.podLogs = this.dashboardService.getPodsLogs('/' + this.groupId, '/' + this.clusterId, '/' + this.podName);
+        saveAs.saveAs(this.podLogs, `${this.podName}.log`);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   AddCluster() {
     this.dashboardService.cloneObj = <any>undefined;
     this.dashboardService.editObj = <any>undefined;
     this.router.navigate(['addcluster']);
   }
-  }
+}
 
--- src/app/service/dashboard.service.ts
+++ src/app/service/dashboard.service.ts
@@ -35,4 +35,12 @@ export class DashboardService {
   getPods(groupId: string, clusterId: string, populate?: string) {
     return this.http.get<Partial<Idashboard>>(this.constantService.get_api_url(this.constantService.API_ENDPOINT + groupId + clusterId + '/pods' + `${populate ? '?populate=' + populate : ''}`));
   }
+  getPodsLogs(groupId: string, clusterId: string, podName: string, populate?: string) {
+    let urlLog = this.constantService.API_ENDPOINT + groupId + clusterId + podName;
+    // console.log(url);
+    // return this.http.get(this.constantService.API_ENDPOINT + groupId + clusterId + podName + `${populate ? '?populate=' + populate : ''}`, {responseType: 'text'});
+    return urlLog;
+
+
+  }
 }
